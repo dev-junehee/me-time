67010b633bbe344179786f3e29a49ea0377171cb
dev-junehee <dev.junehee@gmail.com>
2024-09-11 08:48:16 +0900

    Initial Commit

---
+++ me-time/me_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  me_timeApp.swift
+//  me-time
+//
+//  Created by junehee on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct me_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
