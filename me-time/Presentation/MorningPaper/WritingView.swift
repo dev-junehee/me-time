c834fb4876c5fb03d3657536a42db64083127d80
dev-junehee <dev.junehee@gmail.com>
2024-10-25 15:08:52 +0900

    Fix: 모닝페이퍼 작성 화면 - TextField Placeholder 부분 탭 이벤트 발생 시 TextField 활성화되지 않는 오류 수정

--- me-time/Presentation/MorningPaper/WritingView.swift
+++ me-time/Presentation/MorningPaper/WritingView.swift
@@ -127,14 +127,17 @@ struct WritingView: View {
             /// background 라운드 처리
             RoundedRectangle(cornerRadius: 8)
                 .stroke(.primaryGray, lineWidth: 1)
+            
             /// 실제 컨텐츠 부분
             TextEditor(text: $contentText)
                 .font(.gowunRegular14)
+                .background(.clear)
                 .overlay(alignment: .topLeading) {
                     Text(Constant.MorningPaper.contentPlaceholder)
                         .foregroundStyle(contentText.isEmpty ? .gray : .clear)
                         .font(.gowunRegular12)
                         .padding(10)
+                        .allowsHitTesting(false)
                 }
                 .border(.primaryGray)
                 .clipShape(.rect(cornerRadius: 10))
